3ef08b355ec607f78c4c487378d0c5e4241e63f4
Enzo Falcon <falcone134@gmail.com>
2022-08-17 19:47:42 -0500

    Add forum template

--- src/pages/Forum/Forum.vue
+++ src/pages/Forum/Forum.vue
@@ -1,3 +1,32 @@
 <template>
-  <p>Foro</p>
+  <div class="flex flex-col-reverse md:flex-row gap-4">
+    <div class="flex flex-col gap-2 basis-3/4">
+      <PostResume v-for="i in 5" :key="'post-'+i" />
+    </div>
+    <div class="basis-1/4">
+      <div class="mb-6">
+        <button class="btn-primary w-full">+ Crear nuevo post</button>
+      </div>
+      <form class="rounded bg-white border-[1px] border-gray-300 shadow-md px-4 py-2">
+        <h1 class="mb-2 font-bold">Cursos</h1>
+        <div class="flex flex-col gap-1">
+          <div v-for="(course, index) in courses" :key="index">
+            <input class="mr-4" type="radio" :name="course" :id="course" :value="index" v-model="checked">
+            <label :for="course">{{ course }}</label>
+          </div>
+          <div class="text-center mt-4">
+            <input class="btn-primary" type="submit" value="Actualizar">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import PostResume from '@/components/PostResume.vue'
+import { ref } from 'vue';
+
+const checked = ref<number>(0)
+const courses: string[] = ['Todos', 'Aritmética', 'Geometría', 'Trigonometría', 'Raz. Mat.']
+</script>
