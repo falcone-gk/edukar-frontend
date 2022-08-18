88c370deb551101cc97b8b908cd38757825c9fed
Enzo Falcon <falcone134@gmail.com>
2022-08-17 21:27:30 -0500

    Change border color to primary color

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import routes from './routes/index'
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
-  linkActiveClass: 'flex justify-center items-center text-black h-full w-full border-b-4 border-b-indigo-500'
+  linkActiveClass: 'flex justify-center items-center text-black h-full w-full border-b-4 border-b-primary-color'
 })
 
 export default router
