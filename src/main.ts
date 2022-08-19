19ddb8f2bc13eacd3f700d6c51c5ffb20e5d1682
Enzo Falcon <falcone134@gmail.com>
2022-08-19 18:33:12 -0500

    Add font-awesome-icon

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import router from './router'
 import { createPinia } from 'pinia'
 import './index.css'
 
+/**Adding font-awsome-icon component */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/**Import icons */
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faXmark)
+
 const pinia = createPinia()
 const app = createApp(App)
 
+app.component('font-icon', FontAwesomeIcon)
 app.use(router)
 app.use(pinia)
 app.mount('#app')
