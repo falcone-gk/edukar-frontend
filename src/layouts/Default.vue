650725a382f31e3f15dbde454c353a3da70ec4f6
Enzo Falcon <falcone134@gmail.com>
2022-08-23 17:51:59 -0500

    Add margins in main container

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
-  <main class="container mx-auto mt-6 px-3">
+  <main class="container mx-auto my-12 px-3">
     <router-view />
   </main>
+  <Footer />
 </template>
 
 <script lang="ts" setup>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
