9ff761422722a72ed5fb7f633bb44f6f22a51f4f
Enzo Falcon <falcone134@gmail.com>
2022-08-23 19:13:53 -0500

    Create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <footer class="mt-auto bg-primary-color text-gray-100 pt-8 pb-2 px-6">
+    <div class="flex flex-col h-full w-full text-center md:text-left gap-10 text-sm md:flex-row">
+      <div class="flex-grow">
+        <h1 class="mb-4">EDUKAR</h1>
+        <p>En este foro podrás compartir con la comunidad tus consultas de ejercicios, resoluciones, información y
+          bibliografía sobre los diferentes cursos académicos y así apoyarnos entre todos a mejorar nuestro desempeño
+          estudiantil. </p>
+      </div>
+      <div class="flex-grow">
+        <h1 class="mb-4">SERVICIOS</h1>
+        <p>En este foro podrás compartir con la comunidad tus consultas de ejercicios, resoluciones, información y
+          bibliografía sobre los diferentes cursos académicos y así apoyarnos entre todos a mejorar nuestro desempeño
+          estudiantil. </p>
+      </div>
+      <div class="flex-grow">
+        <h1 class="mb-4">COMPAÑÍA</h1>
+        <p>En este foro podrás compartir con la comunidad tus consultas de ejercicios, resoluciones, información y
+          bibliografía sobre los diferentes cursos académicos y así apoyarnos entre todos a mejorar nuestro desempeño
+          estudiantil. </p>
+      </div>
+      <div class="flex-grow whitespace-nowrap">
+        <h1 class="mb-4">CONTACTO</h1>
+        <p>
+          <font-icon icon="fa-solid fa-home" /> Urb. Juan XXIII LT. 10 Mz. B Dpto. C3
+        </p>
+        <p>
+          <font-icon icon="fa-solid fa-envelope" /> aedukar@gmail.com
+        </p>
+        <p>
+          <font-icon icon="fa-solid fa-phone" /> +51 924 271 370
+        </p>
+      </div>
+    </div>
+    <hr class="w-full mt-6 mb-3">
+    <div class="flex flex-col gap-3 md:flex-row">
+      <div class="flex justify-center items-center">
+        © 2020 Copyright:&nbsp; <a class="underline" href="http://aedukar.com/">aedukar.com</a>
+      </div>
+      <div class="flex gap-1 justify-center items-center md:ml-auto">
+        <a href="#" class="border rounded border-white transition-colors hover:transform hover:bg-indigo-200 hover:text-black py-1 px-3">
+          <font-icon icon="fa-brands fa-facebook-f" />
+        </a>
+        <a href="#" class="border rounded border-white transition-colors hover:transform hover:bg-indigo-200 hover:text-black py-1 px-3">
+          <font-icon icon="fa-brands fa-twitter" />
+        </a>
+        <a href="#" class="border rounded border-white transition-colors hover:transform hover:bg-indigo-200 hover:text-black py-1 px-3">
+          <font-icon icon="fa-brands fa-youtube" />
+        </a>
+        <a href="#" class="border rounded border-white transition-colors hover:transform hover:bg-indigo-200 hover:text-black py-1 px-3">
+          <font-icon icon="fa-brands fa-instagram" />
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
