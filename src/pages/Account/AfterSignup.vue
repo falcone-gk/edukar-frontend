978f46c6d0f42fc1dbcad51beaae86b816a9bea2
Enzo Falcon <falcone134@gmail.com>
2022-09-05 18:57:01 -0500

    Add resend email verification

--- src/pages/Account/AfterSignup.vue
+++ src/pages/Account/AfterSignup.vue
@@ -1,12 +1,27 @@
 <template>
   <Announcement :title="title" :imageName="imgSrc">
-    <p class="mb-4">Felicidades por registrarte a Edukar, para poder logearte tienes que verificar tu cuenta, para ello te hemos enviado un email a tu correo.</p>
+    <p class="mb-4">Felicidades por registrarte a Edukar, para poder logearte tienes que verificar tu cuenta, para ello
+      te hemos enviado un email a tu correo <span class="text-primary-color">{{ authStore.email }}</span>.</p>
+    <p>¿No te llegó el link de verificación? Recuerda verificar en spam o dar click al siguiente botón para volver a
+      enviar el link:</p>
+    <button @click.prevent="resendEmailVerification" class="btn-primary mt-4">Reenviar link de verificación</button>
   </Announcement>
 </template>
 
 <script lang="ts" setup>
 import Announcement from '@/components/Announcement.vue'
+import httpModule from '../../services/httpModule';
+import { useAuthStore } from '../../stores/auth';
 
 const title: string = 'Verificación de email'
 const imgSrc: string = 'message.png'
+const authStore = useAuthStore()
+
+const resendEmailVerification = () => {
+  try {
+    const response = httpModule.post('account/users/resend_activation/')
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
