c332563e5c988315f5cef90c11849ac026519544
Enzo Falcon <falcone134@gmail.com>
2022-09-09 00:22:28 -0500

    Add saveUserData action

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -3,12 +3,22 @@ import { userData } from '../types/authTypes'
 import router from '../router'
 import httpModule from '../services/httpModule'
 
+// Not including isAuthenticated since is boolean.
+export interface userDataStorage {
+    access: string,
+    refresh: string
+    username: string,
+    email: string,
+    picture: string
+}
+
 export const useAuthStore = defineStore('auth', {
     state: (): userData => {
         return {
             isAuthenticated: false,
-            token: null,
-            username: null,
+            access: '',
+            refresh: '',
+            username: '',
             email: 'example@example.com',   // Using this value just to render in view
             picture: '',
         }
@@ -23,5 +33,20 @@ export const useAuthStore = defineStore('auth', {
                 console.log(error)
             }
         },
+        async saveUserData(data: userData) {
+            this.$patch(data)
+            this.isAuthenticated = true
+            Object.keys(data).forEach((key: string) => {
+                localStorage.setItem(key, data[key as keyof userDataStorage])
+            });
+        },
+        async updateAccessToken(token: string) {
+            this.access = token
+            localStorage.setItem('access', token)
+        },
+        async logout() {
+            this.$reset()
+            localStorage.clear()
+        }
     }
 })
