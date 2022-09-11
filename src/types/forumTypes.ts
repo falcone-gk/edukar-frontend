a226693c9ac701ecd7505ff675f2c8762d8aab90
Enzo Falcon <falcone134@gmail.com>
2022-09-11 00:58:24 -0500

    Create forum types

---
+++ src/types/forumTypes.ts
@@ -0,0 +1,13 @@
+export interface postInfoResume {
+    title: string,
+    slug: string,
+    author: string,
+    picture: string,
+    time_difference: string,
+    subsection: string
+}
+
+export interface subsectionType {
+    id: number,
+    name: string
+}
