aacdee4731fee4b4be6653ae950193c01b4473ce
Enzo Falcon <falcone134@gmail.com>
2022-09-20 10:31:05 -0500

    Post components moved

---
+++ src/components/PostComponents/PostComment.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="comment my-3">
+    <div class="flex items-center mb-4">
+      <div class="pr-2">
+        <img class="rounded-full w-[48px] h-[48px] max-w-none" :src="Args.props.author.picture" alt="picture">
+      </div>
+      <div>
+        <h1 class="text-md text-primary-color">{{ Args.props.author.username }}</h1>
+        <p class="text-xs text-gray-600">Publicado el <span>{{ Args.props.date }}</span> </p>
+      </div>
+    </div>
+    <div class="text-sm" v-html="Args.props.body"></div>
+    <div class="my-2">
+      <button @click="isReplyActive = !isReplyActive" class="text-xs text-slate-700 rounded p-1 hover:bg-indigo-100">
+        <font-icon icon="fa-solid fa-reply" />
+        <span> Responder</span>
+      </button>
+    </div>
+
+    <div v-if="isReplyActive" class="new-reply-form my-3">
+      <h2 class="mb-2">Responde al comentario:</h2>
+      <QuillEditor v-model:content="replyForm.body" contentType="html" />
+      <button @click.prevent="replyComment" class="btn-primary mt-2">Responder</button>
+    </div>
+
+    <div v-if="Args.props.replies.length" class="replies ml-8">
+      <h2>Respuestas:</h2>
+      <PostReply v-for="reply in Args.props.replies"
+      :key="'reply-' + reply.id"
+      :Args="{props: reply}" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import httpModule from '../../services/httpModule';
+import PostReply from '@/components/PostComponents/PostReply.vue';
+import { commentStructure } from '../../types/forumTypes';
+
+interface Props {
+  Args: {props: commentStructure},
+  postId?: number,
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+
+// value used to toggle reply to comment box
+const isReplyActive = ref<boolean>(false)
+
+const replyForm = reactive({
+  post: props.postId,
+  comment: props.Args.props.id,
+  body: ''
+})
+const replyComment = async () => {
+  const response = await httpModule.post('forum/replies/', replyForm)
+  console.log(response.data)
+}
+</script>
---
+++ src/components/PostComponents/PostReply.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="comment my-3">
+    <div class="flex items-center mb-4">
+      <div class="pr-2">
+        <img class="rounded-full w-[48px] h-[48px] max-w-none" :src="Args.props.author.picture" alt="picture">
+      </div>
+      <div>
+        <h1 class="text-md text-primary-color">{{ Args.props.author.username }}</h1>
+        <p class="text-xs text-gray-600">Publicado el <span>{{ Args.props.date }}</span> </p>
+      </div>
+    </div>
+    <div class="text-sm" v-html="Args.props.body"></div>
+  </div>
+</template>
+<script setup lang="ts">
+import { commentStructure } from '../../types/forumTypes';
+
+interface Props {
+  Args: {props: commentStructure},
+  postId?: number,
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+
+</script>
