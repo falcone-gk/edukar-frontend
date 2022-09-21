3814e46512393ebddfa847e0b6a622ef78b26fab
Enzo Falcon <falcone134@gmail.com>
2022-09-21 12:47:25 -0500

    Set authStore state when app is refreshed

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
   <router-view />
 </template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { userData } from './types/authTypes'
+import { useAuthStore } from './stores/auth'
+
+const authStore = useAuthStore()
+
+onBeforeMount(() => {
+  const userLocalData: userData | {} = JSON.parse(localStorage.getItem('userData') || '{}')
+  if (Object.keys(userLocalData).length === 0) {
+    authStore.logout()
+  } else {
+    authStore.saveUserData(userLocalData as userData)
+  }
+})
+</script>
