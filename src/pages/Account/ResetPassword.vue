4462a6835017ac56acaddb59b0951d5a9d3e8468
Enzo Falcon <falcone134@gmail.com>
2022-09-23 13:47:09 -0500

    Add modal when success or error

--- src/pages/Account/ResetPassword.vue
+++ src/pages/Account/ResetPassword.vue
@@ -1,4 +1,9 @@
 <template>
+  <ModalDialog
+  :show="modalData.showModal"
+  :title="modalData.title"
+  :message="modalData.message"
+  @closeModal="() => {modalData.showModal = false}" />
   <div class="grid place-items-center">
     <div class="w-[350px] p-4 border-t border-t-gray-100 shadow-md">
       <div class="mb-4 w-full">
@@ -9,23 +14,33 @@
           <label class="label-control" for="email">Email</label>
           <input id="email" class="form-control" type="text" name="email" v-model="email">
         </div>
-        <input class="w-full btn-primary" type="submit" @click="sendUrlResetPassword" value="Enviar link de recuperación">
+        <input class="w-full btn-primary" type="submit" @click.prevent="sendUrlResetPassword" value="Enviar link de recuperación">
       </form>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import httpModule from '../../services/httpModule';
+import ModalDialog from '../../components/ModalDialog.vue';
 
+const modalData = reactive({
+  showModal: false,
+  title: '',
+  message: ''
+})
 const email = ref<string>('')
-const sendUrlResetPassword = () => {
+const sendUrlResetPassword = async () => {
   try {
-    const response = httpModule.post('account/users/reset_password/', {'email': email})
-    console.log(response)
+    await httpModule.post('account/users/reset_password/', {'email': email.value})
+    modalData.showModal = true
+    modalData.title = 'Restablecer contraseña'
+    modalData.message = `Se ha enviado un correo a ${email.value} para restablecer su contraseña de manera exitosa. Revisar su bandeja de entrada.`
   } catch(error) {
-    console.log(error)
+    modalData.showModal = true
+    modalData.title = 'Restablecer contraseña'
+    modalData.message = `Hubo un problema al enviar el correo a ${email.value}. Esto puede deberse a que dicho correo no está registrado.`
   }
 }
 </script>
